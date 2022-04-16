48b539f430a1d9603f4dfe37973bb74dcf9fbf22
Noha M <33437197+NohaaAa@users.noreply.github.com>
2022-04-16 20:38:36 +0200

    created the app with next & react & typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  poweredByHeader: false,
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+    buildActivity: true,
+  },
+  i18n: {
+    locales: ['default', 'en', 'ar'],
+    defaultLocale: 'default',
+    localeDetection: false,
+  },
+  trailingSlash: false,
+  images: {
+    // domains: ['cdn-stg-revamp.almentor.net'],
+  },
+};
