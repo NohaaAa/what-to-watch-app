e82e0c51015a928fdb687ac5cc763fd003e190f3
Noha M <33437197+NohaaAa@users.noreply.github.com>
2022-04-25 17:25:35 +0200

    uploaded all thumbnails to uploads folder & added the rest of movie routes & added morgan to log server requests

--- what-to-watch-server/routes/movies.js
+++ what-to-watch-server/routes/movies.js
@@ -3,19 +3,56 @@ const Movie = require("../models/Movie");
 const {verifyTokenAndAuthorization} = require("../verifyToken");
 
 //CREATE new movie (one or list)
-router.post('/', verifyTokenAndAuthorization, async(req, res)=> {
-    // const newMovie = new Movie(req.body);
+router.post('/', async(req, res)=> {
     try {
         const movies = await Movie.insertMany(req.body);
         res.status(201).json(movies);
     }catch(err) {
         res.status(500).json(err);
     }
-})
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+   try {
+       const updateMovie = await Movie.findByIdAndUpdate(
+           req.params.id,
+           {$set: req.body},
+           {new: true});
+       res.status(200).json(updateMovie);
+   }catch (err) {
+       res.status(500).json(err);
+   }
+});
+
 //DELETE delete a movie
+router.put("/:id", async (req, res) => {
+    try {
+        await Movie.findByIdAndDelete(req.params.id);
+        res.status(200).json("The move is successfully deleted!");
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //GET get movie by id
+router.get("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.status(200).json(movie);
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-//GET ALL get all movies
+//GET get all movies
+router.get('/', verifyTokenAndAuthorization, async(req, res)=> {
+    try {
+        const movies = await Movie.find();
+        res.status(201).json(movies);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
--- what-to-watch-server/server.js
+++ what-to-watch-server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const morgan = require('morgan');
 // const uploadImage = require("./routes/image");
 const users = require("./routes/user");
 const auth = require("./routes/auth");
@@ -9,10 +10,13 @@ const movie = require("./routes/movies");
 const bodyParser = require("body-parser");
 
 dotenv.config();
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+app.use(morgan('tiny'));
 
 mongoose
     .connect(process.env.MONGO_URL)
-    .then(() => console.log("DBConnection success!"))
+    .then(() => console.debug("DBConnection success!"))
     .catch((err) => {
         console.log(err);
     });
@@ -22,6 +26,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(express.json());
+
 app.use("/api/auth/", auth);
 app.use("/api/movies/", movie);
 // app.use('/uploads',express.static(__dirname + '/uploads'));
@@ -29,5 +34,5 @@ app.use("/api/movies/", movie);
 app.use("/api/users/", users);
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("server is running");
+    console.debug('App listening on :5000');
 });
